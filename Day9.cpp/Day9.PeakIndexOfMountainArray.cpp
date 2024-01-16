f4f9a9a0e5db54f36e46a6ac11aa803237ec63de
Yukti Batra <122520278+Yukti-Batra@users.noreply.github.com>
2024-01-16 17:52:56 +0530

    Add files via upload

---
+++ Day9.cpp/Day9.PeakIndexOfMountainArray.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+int find_pivot(vector<int> v) {
+	int s = 0, e = v.size() - 1;
+	int mid=s+(e-s)/2;
+	while (s < e)
+	{
+		if(v[mid] < v[mid+1])
+     			s=mid+1;
+    		else
+      			e = mid;
+    
+		mid =s+(e-s)/ 2;
+	}
+	return s;
+    }
+    int peakIndexInMountainArray(vector<int>& arr) {
+     return find_pivot(arr);   
+    }
+};

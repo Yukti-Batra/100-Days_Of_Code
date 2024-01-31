8299445c0835a1d75ffbabb6b486ba0463788ab0
Yukti Batra <122520278+Yukti-Batra@users.noreply.github.com>
2024-01-31 22:14:49 +0530

    Add files via upload

---
+++ Day24.cpp/Day24.RemoveAlloccurrencesofastring.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+// String
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length()!=0 && s.find(part)<s.length()){
+            s.erase(s.find(part),part.length());
+        }
+        return s;
+    }
+};

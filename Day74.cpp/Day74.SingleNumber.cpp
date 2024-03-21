bc82795bab5bcf8d9a6d3287a86dc6597c4c95e4
Yukti Batra <122520278+Yukti-Batra@users.noreply.github.com>
2024-03-21 22:37:29 +0530

    Add files via upload

---
+++ Day74.cpp/Day74.SingleNumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};

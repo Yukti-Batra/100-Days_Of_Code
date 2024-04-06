6dffa48f1890fc5e599017cfa401107e51fd55a3
Yukti Batra <122520278+Yukti-Batra@users.noreply.github.com>
2024-04-06 20:53:01 +0530

    Add files via upload

---
+++ Day90.cpp/Day90.NimGame.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4!=0)
+        return 1;
+        else
+        return 0;
+
+    }
+};
